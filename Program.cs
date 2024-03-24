9a8299f701b6f1e2aa9fa51d67b5569977a5d322
hwangminghung <hwangminghung@gmail.com>
2024-03-24 15:36:27 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] students = { "John", "Jane", "Alice", "Bob", "David" };
+
+        Console.WriteLine("Nhập tên sinh viên cần tìm:");
+        string name = Console.ReadLine();
+
+        int position = Array.IndexOf(students, name);
+
+        if (position != -1)
+        {
+            Console.WriteLine("Vị trí của sinh viên " + name + " trong danh sách là " + (position + 1));
+        }
+        else
+        {
+            Console.WriteLine("Không tìm thấy sinh viên " + name + " trong danh sách");
+        }
+    }
+}
